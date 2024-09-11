9b8c4f46e227a7851e4e626bfdc18a12e0977789
Lucas <lucasfreiresabino@gmail.com>
2024-09-11 11:28:16 -0600

    Testing

--- trilingualDemocracy.js
+++ trilingualDemocracy.js
@@ -5,7 +5,7 @@ function trilingualDemocracy(group) {
     let scoreF = 0;
     let scoreI = 0;
     let scoreK = 0;
-    const array = [scoreD, scoreF, scoreI, scoreK];
+
   for(var i = 0; i < group.length(); i++){
     if(group[i] === 'D'){
         scoreD += 1;
@@ -13,9 +13,29 @@ function trilingualDemocracy(group) {
         scoreF += 1;
     }else if(group[i] === 'I'){
         scoreI += 1;
-    }else if(group[i] === 'F'){
-
+    }else if(group[i] === 'K'){
+        scoreK += 1;
     }
+
+  }
+
+  if(scoreD >= 2){
+    return 'D';
+  }else if(scoreF >= 2){
+    return 'F'
+  }else if(scoreI >= 2){
+    return 'I'
+  }else if(scoreK >= 2){
+    return 'K'
+  }else if(scoreD === 0){
+    return 'D';
+  }else if(scoreF === 2){
+    return 'F'
+  }else if(scoreI === 2){
+    return 'I'
+  }else if(scoreK === 2){
+    return 'K'
+  }
 
   }
   
